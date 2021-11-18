ef7918776675b84f3ac91821c6bd0e0b42840f47
molechi <mo.lechthaler@gmx.at>
2021-11-18 12:38:11 +0100

    initial commit

---
+++ NumberGuess/NumberGuess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumberGuess
+//
+//  Created by Moritz Lechthaler on 18.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- NumberGuesser_2/NumberGuesser_2/NumberGuessModel.swift
+++ NumberGuesser_2/NumberGuesser_2/NumberGuessModel.swift
@@ -1,30 +1,8 @@
-import Foundation
+//
+//  NumberGuessModel.swift
+//  NumberGuesser_2
+//
+//  Created by Moritz Lechthaler on 18.11.21.
+//
 
-class NumberGuessModel {
-    
-    var target = 0
-    var guessCount = 0
-    
-    func compare(to: Int) -> Int {
-        return target - to
-    }
-    
-    func isValid(string: String?) -> Bool {
-        var isValid = false
-        if let str = string, let guess = Int(str) {
-            if guess >= 1 && guess <= 99 {
-                isValid = true
-            }
-        }
-        return isValid
-    }
-    
-    func getImage(bool: Bool) -> Bool{
-        var getImage = false
-        
-        
-        
-        return getImage
-    }
-    
-}
+import Foundation
